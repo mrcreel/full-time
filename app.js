cb3c18f4709b328e6e72a894c9269f1329e445df
mrcreel <michaelrcreel@gmail.com>
2019-02-16 22:35:54 -0600

    Set-up basic grapiql interface

--- app.js
+++ app.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
-const PORT =3000
+const graphQlHTTP = require('express-graphql')
+const {buildSchema} = require('graphql')
 
 const app = express()
 
+const GRAPHQL_PORT =3000
+
 app.use(bodyParser.json())
 
-app.get('/', (req, res, next)=> {
-  console.log(`Connected to localhost:${PORT}`)
-  res.send('FULL-TIME')
-})
+app.use('/graphql', graphQlHTTP({
+  schema: buildSchema(`
+    type Query{
+      leagues: [String!]!
+    }
+    type Mutation{
+      createLeague(name: String): String
+    }
+    schema{
+      query: Query
+      mutation: Mutation
+    }
+  `),
+  rootValue: {
+    leagues: () => {
+      return [
+        'Premier League',
+        'Championship',
+        'League One',
+        'League Two',
+      ]
+    },
+    createLeague: (args) => {
+      const leagueName = args.name
+      return leagueName
+    }
+  },
+  graphiql: true
+}))
 
-app.listen(PORT)
+app.listen(GRAPHQL_PORT)
